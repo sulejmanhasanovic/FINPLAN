926f424e46d6b69b15ce1e0eddabbd0594a713d7
sulejmanh <sulejman.hasanovic@gmail.com>
2021-04-09 23:03:46 +0200

    Case study id in calculation

--- app/calculation/calculation.php
+++ app/calculation/calculation.php
@@ -10,7 +10,7 @@
 	//ini_set('display_startup_errors', 1);
 	//ini_set('display_errors', 1);
 	//error_reporting(-1);
-	$finplanService = new FinplanService($_SESSION['cs']['id']);
+	$finplanService = new FinplanService($caseStudyId);
 	$finplanService->runCalculations();
 
 
